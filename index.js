949062ff9c0302d62dda9cc90fce9b35b0b357a1
dinushchathurya <dinushchathurya21@gmail.com>
2022-05-20 22:19:27 +0530

    feat: Finish workflow

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs").promises;
 
 const axios = require("axios");
 
-const readme_path = core.getInput("readme_path", { required: true });
+const readme_path = core.getInput('readme_path') || "README.md";
 
 (async () => {
   try {
