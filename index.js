41556e2a1c82dee1629107b2782efffc4e769b3f
dinushchathurya <dinushchathurya21@gmail.com>
2022-05-20 22:25:25 +0530

    feat: Finish workflow

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ const readme_path = core.getInput('readme_path') || "README.md";
 
 (async () => {
   try {
-    const { data } = await axios.get(
-      `https://api.npoint.io/70a5424aeed887821469`
-    );
+
+    const { data } = await axios.get(`https://api.npoint.io/70a5424aeed887821469`);
 
     let quote = `<!-- start quote -->\n`;
     let qotd = data.contents.quotes[0].quote;
@@ -20,6 +19,7 @@ const readme_path = core.getInput('readme_path') || "README.md";
     const newText = currentText.replace(quoteSection, quote);
 
     await fs.writeFile(readme_path, newText);
+
   } catch (error) {
     console.log(error.message);
   }
